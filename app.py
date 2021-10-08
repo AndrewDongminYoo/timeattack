3bddd8f1ca39e2abc184173e8849aa5d691cde7f
AndrewDongminYoo <ydm2790@gmail.com>
2021-10-08 18:52:17 +0900

    time format

--- app.py
+++ app.py
@@ -17,11 +17,13 @@ def index():
 
 @app.route('/post', methods=['POST'])
 def save_post():
-    idx = len(list(col.find({})))
+    idx = col.count({})+1
     title = request.form.get('title')
     content = request.form.get('content')
-    reg_date = datetime.datetime.now()
-    doc = {"idx": idx, 'title': title, 'content': content, 'reg_date': reg_date}
+    reg_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    mod_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    read = 0
+    doc = {"idx": idx, 'title': title, 'content': content, 'reg_date': reg_date, "mod_date": mod_date, 'read': read}
     col.insert(doc)
     return jsonify({"result": "success", "uploaded": idx})
 
@@ -39,6 +41,23 @@ def delete_post():
     contents = list(col.find({}, {"_id": False}).sort("reg_date", -1))
     print(contents)
     return jsonify({"result": "success", "contents": contents})
+
+
+@app.route('/detail/<idx>', methods=['GET'])
+def read_post(idx):
+    col.update_one({"idx": idx}, {"$inc": {"read": +1}})
+    content = list(col.find({"idx": idx}, {"_id": False}))
+    print(content)
+    return jsonify({"result": "success", "updated": content})
+
+
+@app.route('/update/<idx>', methods=['PUT'])
+def modify_post(idx):
+    title = request.form.get('title')
+    content = request.form.get('content')
+    mod_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    col.update_one({"idx": idx}, {"$set": {"title": title, "content": content, "mod_date": mod_date}})
+    return jsonify({"result": "success", "updated": content})
 
 
 if __name__ == "__main__":
